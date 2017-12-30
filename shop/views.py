60e5aa746f68b5a9bd24597a6853bbffe46b26d6
miskoom <hmiskoom@yahoo.com>
2017-12-30 19:07:52 +0100

    detail view and some minor changes

--- shop/views.py
+++ shop/views.py
@@ -15,10 +15,9 @@ def product_list(request, category_slug=None):
                  'products': products})
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-                                id=id,
-                                slug=slug,
-                                available=True)
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                     'shop/product/detail.html',
-                    {'product':product})
+                    {'product':product,
+                    'cart_product_form': cart_product_form})
